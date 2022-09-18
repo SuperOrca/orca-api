44de206e4607d3dd3ad8eb2f9b3f3e2b2dbc5fdc
SuperOrca <shreyasbhar@gmail.com>
2022-09-18 19:46:43 -0400

    Use uvicorn to deploy :rocket:

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python deploy.py
+web: uvicorn app:app
--- deploy.py
+++
@@ -1,7 +0,0 @@
-import uvicorn
-import colorama
-
-
-if __name__ == "__main__":
-    colorama.init()
-    uvicorn.run("app:app", port=80, log_level="info")
