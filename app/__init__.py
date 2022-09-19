c963a70fba8e71e474282b8c2527ba48b99a9665
SuperOrca <shreyasbhar@gmail.com>
2022-09-18 23:22:57 -0400

    Swagger UI dark theme :children_crossing:

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
+from __future__ import annotations
+
 import toml
-from fastapi import FastAPI, Request
-from fastapi.responses import RedirectResponse
+from fastapi import FastAPI
+from fastapi.openapi.docs import get_swagger_ui_html
 from slowapi import _rate_limit_exceeded_handler
 from slowapi.errors import RateLimitExceeded
 
@@ -10,8 +12,17 @@ from .routers import wyr
 with open("config.toml", "r") as file:
     config = toml.load(file)
 
-app = FastAPI(**config)
+app = FastAPI(openapi_url="/swagger.json", **config)
 app.state.limiter = limiter
 app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 app.include_router(wyr.router)
+
+
+@app.get("/", include_in_schema=False)
+async def index():
+    return get_swagger_ui_html(
+        openapi_url=app.openapi_url,
+        title=f"{app.title} - Swagger UI",
+        swagger_css_url="https://cdn.jsdelivr.net/gh/Itz-fork/Fastapi-Swagger-UI-Dark/assets/swagger_ui_dark.min.css",
+    )
