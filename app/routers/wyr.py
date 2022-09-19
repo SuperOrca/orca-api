0448ed39f02ee1276b0df3dd2067241c63291469
SuperOrca <shreyasbhar@gmail.com>
2022-09-18 20:24:08 -0400

    Add descriptions to routes :art:

--- app/routers/wyr.py
+++ app/routers/wyr.py
@@ -9,16 +9,22 @@ from ..limiter import limiter
 with open("data/wyr.json", "r") as file:
     data = orjson.loads(file.read())
 
-router = APIRouter(prefix="/wyr", tags=["wyr"])
+router = APIRouter(prefix="/wyr", tags=["Would You Rather?"])
 
 
-@router.get("/", response_class=ORJSONResponse)
+@router.get(
+    "/", response_class=ORJSONResponse, description="Get one would you rather question."
+)
 @limiter.limit("3/second")
 async def get_one(request: Request) -> ORJSONResponse:
     return ORJSONResponse({"question": random.choice(data)})
 
 
-@router.get("/{amount}", response_class=ORJSONResponse)
+@router.get(
+    "/{amount}",
+    response_class=ORJSONResponse,
+    description="Get multiple would you rather questions.",
+)
 @limiter.limit("2/second")
 async def get_amount(request: Request, amount: int) -> ORJSONResponse:
     if amount < 1:  # min amount
