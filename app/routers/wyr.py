8cae0fa17c8c39dae162887fd0db140ba1868dfd
SuperOrca <shreyasbhar@gmail.com>
2022-09-18 20:18:26 -0400

    Fix annotated response type :rotating_light:

--- app/routers/wyr.py
+++ app/routers/wyr.py
@@ -14,13 +14,13 @@ router = APIRouter(prefix="/wyr", tags=["wyr"])
 
 @router.get("/", response_class=ORJSONResponse)
 @limiter.limit("3/second")
-async def get_one(request: Request) -> dict:
+async def get_one(request: Request) -> ORJSONResponse:
     return ORJSONResponse({"question": random.choice(data)})
 
 
 @router.get("/{amount}", response_class=ORJSONResponse)
 @limiter.limit("2/second")
-async def get_amount(request: Request, amount: int) -> dict:
+async def get_amount(request: Request, amount: int) -> ORJSONResponse:
     if amount < 1:  # min amount
         raise HTTPException(status_code=400, detail="The amount is less than 1.")
 
